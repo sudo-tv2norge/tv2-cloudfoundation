991a9916e5a57f85666d3f0760eb11029042e984
Maurizio Noseda Pedraglio <maurizio.noseda@gmail.com>
2022-11-15 18:08:24 +0100

    fix to main.tf

--- blueprints/cloud-operations/network-dashboard/main.tf
+++ blueprints/cloud-operations/network-dashboard/main.tf
@@ -168,7 +168,7 @@ module "cloud-function" {
   environment_variables = {
     MONITORED_PROJECTS_LIST = local.projects
     MONITORED_FOLDERS_LIST  = local.folders
-    MONITORING_PROJECT_ID   = local.monitoring_project
+    MONITORING_PROJECT_ID   = local.metrics_project
     ORGANIZATION_ID         = var.organization_id
     CF_VERSION              = var.cf_version
   }
